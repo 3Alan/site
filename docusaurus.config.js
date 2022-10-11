d47a3f24bfed28a04aa5fa07df21213c3a5cb741
3Alan <1540703192@qq.com>
2022-10-11 20:50:23 +0800

    test docsearch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -148,8 +148,31 @@ const config = {
         }
       },
       colorMode: {
-        defaultMode: 'dark'
-      }
+        disableSwitch: true
+      },
+      algolia: {
+        // Algolia 提供的应用 ID
+        appId: '2XRFRLLWRR',
+  
+        //  公开 API 密钥：提交它没有危险
+        apiKey: '9bc14c0a7f5cfdd461f0020fee33bfd9',
+  
+        indexName: 'blog',
+  
+        // 可选：见下文
+        contextualSearch: true,
+  
+        // 可选：声明哪些域名需要用 window.location 型的导航而不是 history.push。 适用于 Algolia 配置会爬取多个文档站点，而我们想要用 window.location.href 在它们之间跳转时。
+        externalUrlRegex: 'external\\.com|domain\\.com',
+  
+        // 可选：Algolia 搜索参数
+        searchParameters: {},
+  
+        // 可选：搜索页面的路径，默认启用（可以用 `false` 禁用）
+        searchPagePath: 'search',
+  
+        // ……其他 Algolia 参数
+      },
     }),
   plugins: [
     [
