b6381d8e60159dc97b1ce0a76ae3cf856fa18b03
3Alan <1540703192@qq.com>
2022-11-06 23:27:37 +0800

    feat: add friends page

--- src/data/friends.ts
+++ src/data/friends.ts
@@ -1,13 +1,15 @@
 export interface FriendItem {
   title: string;
   url: string;
+  avatar?: string;
   description?: string;
   tags?: string[];
 }
 
 const friendList: FriendItem[] = [
   {
-    title: 'Alan Wang',
+    title: 'Alan',
+    avatar: 'https://avatars.githubusercontent.com/u/49978973?v=4',
     url: 'https://www.alanwang.site/',
     description: '此刻想举重若轻，之前必要负重前行',
     tags: ['前端开发']
