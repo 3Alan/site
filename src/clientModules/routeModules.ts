4634e0a033de6fb33314e7b8344b64a2558b6af7
3Alan <1540703192@qq.com>
2023-01-28 17:54:05 +0800

    fix: server error

--- src/clientModules/routeModules.ts
+++ src/clientModules/routeModules.ts
@@ -1,12 +1,17 @@
 import mitt from 'mitt';
+import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 
 const emitter = mitt();
 
-window.emitter = emitter;
+if (ExecutionEnvironment.canUseDOM) {
+  window.emitter = emitter;
+}
 
 export function onRouteDidUpdate() {
+  if (ExecutionEnvironment.canUseDOM) {
+    setTimeout(() => {
+      window.emitter.emit('onRouteDidUpdate');
+    });
+  }
   // https://github.com/facebook/docusaurus/issues/8278
-  setTimeout(() => {
-    window.emitter.emit('onRouteDidUpdate');
-  });
 }
