4f2ffdad9176cfa6873e9b62ea8524ebdc1e43e0
3Alan <1540703192@qq.com>
2023-04-22 15:46:22 +0800

    feat: remove google ads

--- src/plugins/plugin-analytics-inject.js
+++ src/plugins/plugin-analytics-inject.js
@@ -26,15 +26,6 @@ async function analyticsInjectPlugin() {
               src: 'https://analytics.alanwang.site/umami.js',
               'data-website-id': '3c9011ac-8d0f-4d31-a658-8b3806e3d5d4'
             }
-          },
-          // 谷歌广告
-          {
-            tagName: 'script',
-            attributes: {
-              async: true,
-              src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4713657320590781',
-              crossorigin: 'anonymous'
-            }
           }
         ]
       };
