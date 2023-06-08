2490504a5e5cd2608e748fcd8588f25cb98638a8
3Alan <1540703192@qq.com>
2023-06-08 13:14:19 +0800

    feat: clarity

--- src/plugins/plugin-analytics-inject.js
+++ src/plugins/plugin-analytics-inject.js
@@ -25,7 +25,17 @@ async function analyticsInjectPlugin() {
               src: 'https://analytics.alanwang.site/script.js',
               'data-website-id': '3c9011ac-8d0f-4d31-a658-8b3806e3d5d4'
             }
-          }
+          },
+          // 微软 clarity，尝试使用
+          `
+          <script type="text/javascript">
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "hgij2vfn86");
+          </script>
+          `
         ]
       };
     }
