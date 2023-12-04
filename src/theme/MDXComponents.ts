c196103b4ab8183b3998617dfc5ba99f05a139a4
3Alan <1540703192@qq.com>
2023-12-04 23:16:48 +0800

    chore: register some components to global mdx scope

---
+++ src/theme/MDXComponents.ts
@@ -0,0 +1,12 @@
+import MDXComponents from '@theme-original/MDXComponents';
+import Tabs from '@theme/Tabs';
+import TabItem from '@theme/TabItem';
+import Admonition from '@theme/Admonition';
+
+export default {
+  // Re-use the default mapping
+  ...MDXComponents,
+  Tabs,
+  TabItem,
+  Admonition
+};
