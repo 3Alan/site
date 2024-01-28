21ec512389e3c1be8089d265f58b5a625646c420
3Alan <1540703192@qq.com>
2024-01-28 16:12:30 +0800

    feat: i18n

--- src/components/wip/index.tsx
+++ src/components/wip/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import Admonition from '@theme/Admonition';
+import Translate, { translate } from '@docusaurus/Translate';
 
 export function Wip() {
   return (
-    <Admonition type="warning" title="更新中" icon="🚧 🚧">
-      本文还在不断完善更新中...
+    <Admonition
+      type="warning"
+      title={translate({
+        id: 'component.wip.label',
+        message: '更新中'
+      })}
+      icon="🚧 🚧"
+    >
+      <Translate id="component.wip.content">本文还在不断完善更新中...</Translate>
     </Admonition>
   );
 }
