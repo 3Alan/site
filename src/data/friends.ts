faff77cda973d7fe0811d460d14227a5dfeb8e38
3Alan <1540703192@qq.com>
2024-09-19 20:33:50 +0800

    add friend

--- src/data/friends.ts
+++ src/data/friends.ts
@@ -8,26 +8,33 @@ export interface FriendItem {
 
 const friendList: FriendItem[] = [
   {
-    title: 'Alan',
-    avatar: '/img/github_avatar.jpeg',
-    url: 'https://www.alanwang.site/',
-    description: '此刻想举重若轻，之前必要负重前行',
-    tags: ['前端开发']
+    title: "Alan",
+    avatar: "/img/github_avatar.jpeg",
+    url: "https://www.alanwang.site/",
+    description: "此刻想举重若轻，之前必要负重前行",
+    tags: ["前端开发"],
   },
   {
-    title: 'Kuizuo',
-    avatar: '/img/friends/kuizuo.png',
-    url: 'https://kuizuo.cn',
-    description: '记录所学知识，领略编程之美',
-    tags: ['前端开发', 'ts全栈']
+    title: "Kuizuo",
+    avatar: "/img/friends/kuizuo.png",
+    url: "https://kuizuo.cn",
+    description: "记录所学知识，领略编程之美",
+    tags: ["前端开发", "ts全栈"],
   },
   {
-    title: '太空编程',
-    avatar: '/img/friends/spacexcode.png',
-    url: 'https://spacexcode.com',
-    description: '分享硬核的前端编程知识',
-    tags: ['前端开发']
-  }
+    title: "太空编程",
+    avatar: "/img/friends/spacexcode.png",
+    url: "https://spacexcode.com",
+    description: "分享硬核的前端编程知识",
+    tags: ["前端开发"],
+  },
+  {
+    title: "Castamere",
+    avatar: "/img/friends/castamerego.jpeg",
+    url: "https://www.castamerego.com/",
+    description: "代码美学",
+    tags: ["全栈开发", "python开发"],
+  },
 ];
 
 export default friendList;
